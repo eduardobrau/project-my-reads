94ea09667a9217e550ffe582b4734b6c7ce4f910
Esteves <esteves@edusites.com.br>
2017-12-21 14:01:09 -0200

    Feito refatoração da sessão de busca e criado um novo componente src/ui/Search e passado função updateSearchPage() como prop em App, ao clickar em voltar na página do componente Search será disparado um evento que atualizará o state showSearchPage para o inverso do state atual

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as BooksAPI from './apis/BooksAPI';
 import Header from './ui/Header';
 import ListBooks from './ui/ListBooks';
+import Search from './ui/Search';
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -16,6 +17,13 @@ class BooksApp extends React.Component {
 
     books:[]
     
+  }
+
+  updateSearchPage = () => {
+    const condition = this.state.showSearchPage;
+    this.setState(
+      {showSearchPage:(condition) ? false : true}
+    );
   }
 
   componentDidMount(){
@@ -63,26 +71,10 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <Search updateSearchPage={
+            () => this.updateSearchPage()
+            } 
+          />
         ) : (
           <div className="list-books">
             <Header title="MyReads"/>
